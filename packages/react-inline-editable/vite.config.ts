2c8a329d0eb1dde917560a93360a685b955d542e
subhash-malireddy <subhashmalireddy@gmail.com>
2026-01-10 23:12:18 +1100

    add tailwind and downgrade TypeScript to version 5.8.2 to match the vites analysis bundled ts version

--- packages/react-inline-editable/vite.config.ts
+++ packages/react-inline-editable/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import dts from "vite-plugin-dts";
@@ -11,6 +12,7 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     dts({
       tsconfigPath: resolve(__dirname, "tsconfig.build.json"),
       rollupTypes: true,
